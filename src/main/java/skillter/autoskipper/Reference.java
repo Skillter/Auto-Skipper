b6bd82c029e6d0fd5797a817b25659e1412b8a63
Skillter <57605954+Skillter@users.noreply.github.com>
2022-10-11 20:04:51 +0200

    Update Reference.java

--- src/main/java/skillter/autoskipper/Reference.java
+++ src/main/java/skillter/autoskipper/Reference.java
@@ -6,11 +6,6 @@ public class Reference {
     public static final String MOD_NAME = "Auto Skipper";
     public static final String VERSION = "1.0";
     public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.8.9]";
-
-    // Database
-    public static final String[] TABLES = {"skywars", "test"};
-
-    public static final String[] TABLE_SKYWARS = {"skywars", "test"};
-
+    
 
 }
